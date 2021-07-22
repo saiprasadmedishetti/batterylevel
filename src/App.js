4348a3fc812b9503128c81541f71192d88f8fda8
medishetti saiprasad <medishetti.saiprasad@publicissapient.com>
2021-07-22 19:40:23 +0530

    integrated battery level

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [batteryLevel, setBatteryLevel] = useState(0);
+  const [isCharging, setIsCharging] = useState(false);
+
+  const getBattery = useCallback(async () => {
+    if (window.navigator && window.navigator.getBattery) {
+      const battery = await window.navigator.getBattery();
+      if (battery.level !== batteryLevel) {
+        setBatteryLevel(battery.level * 100);
+      }
+      if (battery.charging !== isCharging) {
+        setIsCharging(battery.charging);
+      }
+    }
+  }, [isCharging, batteryLevel]);
+
+  const interval = setInterval(() => {
+    getBattery();
+  }, 1000);
+
+  useEffect(() => {
+    getBattery();
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <div
+        className={`battery-container ${isCharging ? "scale-animation" : ""}`}
+      >
+        <div className="battery-inner">
+          <div className="progress" style={{ width: `${batteryLevel}%` }}></div>
+          <div className="battery-result">{batteryLevel}%</div>
+        </div>
+        <div className="battery-handle"></div>
+      </div>
+    </main>
   );
 }
 
